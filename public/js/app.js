ae305ac38b9f7ebd3f6e1c0e084df02509398d7b
Andy Prasetya <andyprasetya@aol.com>
2018-03-31 16:43:49 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 var map,openTopoMap,openStreetMap,esriWorldImageryMap,zoomControl,attributionControl,scaleControl,layerControl,baseLayers,isCollapsed,
-	earthquakeLayer,pulsingIcon1,pulsingIcon2,pulsingIcon3,mapHash,gridxy,geofongfz_geojson_request,usgs_geojson_request,inasafe_geojson_request,
-	earthquakes,websocket,webSocketShifter;
+	earthquakeLayer,pulsingIcon1,pulsingIcon2,pulsingIcon3,mapHash,gridxy,geofongfzbutton,usgsbutton,inasafebutton,earthquakes,websocket,
+	webSocketShifter;
 var bbox_minx = parseFloat(95), bbox_miny = parseFloat(-11), bbox_maxx = parseFloat(141), bbox_maxy = parseFloat(7.5), 
 	centroidx = parseFloat(122), centroidy = parseFloat(-3), mapminzoom = parseInt(3), mapmaxzoom = parseInt(17), mapinitzoom = parseInt(5);
 openTopoMap = new L.TileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
@@ -39,45 +39,79 @@ if (document.body.clientWidth <= 767) {
 	isCollapsed = false;
 }
 mapHash = new L.Hash(map);
-/* request_geojson_earthquake_geofongfz(); */
-request_geojson_earthquake_usgs();
-/* request_geojson_earthquake_inasafe(); */
+geofongfzbutton = L.easyButton({
+	states: [{
+		stateName: 'geofongfz',
+		icon: 'far fa-dot-circle fa-lg text-geofongfz',
+		title: 'GEOFON GFZ',
+		onClick: function(btn, map) {
+			earthquakeLayer.clearLayers();
+			document.getElementById('data-source').value = 'geofongfz';
+			request_geojson_earthquake();
+			console.log('Get GEOFON GFZ data...');
+			btn.state('geofongfz');
+	}}]
+}).addTo(map);
+usgsbutton = L.easyButton({
+	states: [{
+		stateName: 'usgs',
+		icon: 'far fa-dot-circle fa-lg text-usgs',
+		title: 'USGS',
+		onClick: function(btn, map) {
+			earthquakeLayer.clearLayers();
+			document.getElementById('data-source').value = 'usgs';
+			request_geojson_earthquake();
+			console.log('Get USGS data...');
+			btn.state('usgs');
+	}}]
+}).addTo(map);
+inasafebutton = L.easyButton({
+	states: [{
+		stateName: 'inasafe',
+		icon: 'far fa-dot-circle fa-lg text-inasafe',
+		title: 'InaSAFE',
+		onClick: function(btn, map) {
+			earthquakeLayer.clearLayers();
+			document.getElementById('data-source').value = 'inasafe';
+			request_geojson_earthquake();
+			console.log('Get InaSAFE data...');
+			btn.state('inasafe');
+	}}]
+}).addTo(map);
+request_geojson_earthquake();
 websocket = new WebSocket('ws://localhost:3000');
 webSocketShifter = L.easyButton({
 	states: [{
 		stateName: 'shift-to-websocket',
-		icon: 'fa-toggle-off fa-lg text-default',
+		icon: 'fa-toggle-off fa-lg',
 		title: 'Shift to websocket',
 		onClick: function(btn, map) {
 			earthquakeLayer.clearLayers();
-			/* request_websocket_geojson_earthquake_geofongfz(websocket); */
-			request_websocket_geojson_earthquake_usgs(websocket);
-			/* request_websocket_geojson_earthquake_inasafe(websocket); */
+			request_websocket_geojson_earthquake(websocket);
 			console.log('Shift to websocket...');
 			btn.state('shift-to-onetime');
 	}
 	}, {
 		stateName: 'shift-to-onetime',
-		icon: 'fa-toggle-on fa-lg text-primary',
+		icon: 'fa-toggle-on fa-lg text-websocket',
 		title: 'Turn-off websocket',
 		onClick: function(btn, map) {
 			_send_websocket_termination(websocket);
 			earthquakeLayer.clearLayers();
-			/* request_geojson_earthquake_geofongfz(); */
-			request_geojson_earthquake_usgs();
-			/* request_geojson_earthquake_inasafe(); */
+			request_geojson_earthquake();
 			console.log('Turn-off websocket, request one-time data...');
 			btn.state('shift-to-websocket');
 	}
 	}]
 });
 webSocketShifter.addTo(map);
-function request_geojson_earthquake_geofongfz () {
-	geofongfz_geojson_request = new XMLHttpRequest();
-	geofongfz_geojson_request.open('GET', './latest-geofongfz', true);
-	geofongfz_geojson_request.onreadystatechange = function() {
-		if(geofongfz_geojson_request.readyState === 4) {
-			if(geofongfz_geojson_request.status === 200) {
+function request_geojson_earthquake () {
+	var context = document.getElementById('data-source').value;
+	var geojson_request = new XMLHttpRequest();
+	geojson_request.open('GET', './latest-'+context+'', true);
+	geojson_request.onreadystatechange = function() {
+		if(geojson_request.readyState === 4) {
+			if(geojson_request.status === 200) {
 				var data = JSON.parse(this.responseText);
 				earthquakes = L.geoJson(null, {
 					pointToLayer: function (feature, latlng) {
@@ -98,128 +132,17 @@ function request_geojson_earthquake_geofongfz () {
 			}
 		}
 	}
-	geofongfz_geojson_request.send();
+	geojson_request.send();
 }
-function request_geojson_earthquake_usgs () {
-	usgs_geojson_request = new XMLHttpRequest();
-	usgs_geojson_request.open('GET', './latest-usgs', true);
-	usgs_geojson_request.onreadystatechange = function() {
-		if(usgs_geojson_request.readyState === 4) {
-			if(usgs_geojson_request.status === 200) {
-				var data = JSON.parse(this.responseText);
-				earthquakes = L.geoJson(null, {
-					pointToLayer: function (feature, latlng) {
-						if (parseFloat(feature.properties.mag)<=2.5) {
-							return L.marker(latlng, {icon: pulsingIcon1});
-						} else if (parseFloat(feature.properties.mag)>2.5 && parseFloat(feature.properties.mag)<=4.0) {
-							return L.marker(latlng, {icon: pulsingIcon2});
-						} else {
-							return L.marker(latlng, {icon: pulsingIcon3});
-						}
-					}
-				});
-				earthquakes.addData(data);
-				earthquakeLayer.addLayer(earthquakes);
-			} else {
-				earthquakeLayer.clearLayers();
-				alert('Local GeoJSON fails.');
-			}
-		}
-	}
-	usgs_geojson_request.send();
-}
-function request_geojson_earthquake_inasafe () {
-	inasafe_geojson_request = new XMLHttpRequest();
-	inasafe_geojson_request.open('GET', './latest-inasafe', true);
-	inasafe_geojson_request.onreadystatechange = function() {
-		if(inasafe_geojson_request.readyState === 4) {
-			if(inasafe_geojson_request.status === 200) {
-				var data = JSON.parse(this.responseText);
-				earthquakes = L.geoJson(null, {
-					pointToLayer: function (feature, latlng) {
-						if (parseFloat(feature.properties.mag)<=2.5) {
-							return L.marker(latlng, {icon: pulsingIcon1});
-						} else if (parseFloat(feature.properties.mag)>2.5 && parseFloat(feature.properties.mag)<=4.0) {
-							return L.marker(latlng, {icon: pulsingIcon2});
-						} else {
-							return L.marker(latlng, {icon: pulsingIcon3});
-						}
-					}
-				});
-				earthquakes.addData(data);
-				earthquakeLayer.addLayer(earthquakes);
-			} else {
-				earthquakeLayer.clearLayers();
-				alert('Local GeoJSON fails.');
-			}
-		}
-	}
-	inasafe_geojson_request.send();
-}
-function request_websocket_geojson_earthquake_geofongfz (websocket) {
+function request_websocket_geojson_earthquake (websocket) {
+	var endpointdatasource = document.getElementById('data-source').value;
 	if (websocket.readyState === websocket.OPEN) {
-		websocket.send('request_websocket_data_geofongfz');
+		websocket.send('request_websocket_data_'+endpointdatasource+'');
 	} else {
 		websocket = null;
 		websocket = new WebSocket('ws://localhost:3000');
 		websocket.onopen = function (event) {
-			websocket.send('request_websocket_data_geofongfz');
-		};
-	}
-	websocket.onmessage = function (event) {
-		earthquakeLayer.clearLayers();
-		var data = JSON.parse(event.data);
-		earthquakes = L.geoJson(null, {
-			pointToLayer: function (feature, latlng) {
-				if (parseFloat(feature.properties.mag)<=2.5) {
-					return L.marker(latlng, {icon: pulsingIcon1});
-				} else if (parseFloat(feature.properties.mag)>2.5 && parseFloat(feature.properties.mag)<=4.0) {
-					return L.marker(latlng, {icon: pulsingIcon2});
-				} else {
-					return L.marker(latlng, {icon: pulsingIcon3});
-				}
-			}
-		});
-		earthquakes.addData(data);
-		earthquakeLayer.addLayer(earthquakes);
-	};
-}
-function request_websocket_geojson_earthquake_usgs (websocket) {
-	if (websocket.readyState === websocket.OPEN) {
-		websocket.send('request_websocket_data_usgs');
-	} else {
-		websocket = null;
-		websocket = new WebSocket('ws://localhost:3000');
-		websocket.onopen = function (event) {
-			websocket.send('request_websocket_data_usgs');
-		};
-	}
-	websocket.onmessage = function (event) {
-		earthquakeLayer.clearLayers();
-		var data = JSON.parse(event.data);
-		earthquakes = L.geoJson(null, {
-			pointToLayer: function (feature, latlng) {
-				if (parseFloat(feature.properties.mag)<=2.5) {
-					return L.marker(latlng, {icon: pulsingIcon1});
-				} else if (parseFloat(feature.properties.mag)>2.5 && parseFloat(feature.properties.mag)<=4.0) {
-					return L.marker(latlng, {icon: pulsingIcon2});
-				} else {
-					return L.marker(latlng, {icon: pulsingIcon3});
-				}
-			}
-		});
-		earthquakes.addData(data);
-		earthquakeLayer.addLayer(earthquakes);
-	};
-}
-function request_websocket_geojson_earthquake_inasafe (websocket) {
-	if (websocket.readyState === websocket.OPEN) {
-		websocket.send('request_websocket_data_inasafe');
-	} else {
-		websocket = null;
-		websocket = new WebSocket('ws://localhost:3000');
-		websocket.onopen = function (event) {
-			websocket.send('request_websocket_data_inasafe');
+			websocket.send('request_websocket_data_'+endpointdatasource+'');
 		};
 	}
 	websocket.onmessage = function (event) {
